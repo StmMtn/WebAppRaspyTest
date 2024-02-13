f6fd575e3f855d654a9c2cfa5670f65c851c88c3
Flaig <moritz.flaig@zeiss.com>
2024-02-13 16:10:09 +0100

    edited

--- Test.py
+++ Test.py
@@ -1,12 +1,10 @@
 from samplebase import SampleBase
 
 class RotatingBlockGenerator(SampleBase):
-
-    def __init__(self, x,y):
-        super(RotatingBlockGenerator, self).__init__(32, 32)
     def run(self):
         offset_canvas.SetPixel(1, 1, 0, 255, 0)
         offset_canvas = self.matrix.SwapOnVSync(offset_canvas)
         
 if __name__ == "__main__":
-    rotating_block_generator = RotatingBlockGenerator(x=32,y=32)
+    rotating_block_generator = RotatingBlockGenerator()
+    rotating_block_generator.run
